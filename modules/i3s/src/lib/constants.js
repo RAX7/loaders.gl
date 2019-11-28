98528007688adb002888a8085980c66039696748
Xintong Xia <xintong@uber.com>
2019-11-27 16:14:46 -0800

    remove unused code

--- modules/i3s/src/lib/constants.js
+++ modules/i3s/src/lib/constants.js
@@ -1,26 +1,4 @@
-// TILE TYPES
-
-export const TILE3D_TYPE = {
-  COMPOSITE: 'cmpt',
-  POINT_CLOUD: 'pnts',
-  BATCHED_3D_MODEL: 'b3dm',
-  INSTANCED_3D_MODEL: 'i3dm',
-  GEOMETRY: 'geom',
-  VECTOR: 'vect'
-};
-
-export const TILE3D_TYPES = Object.keys(TILE3D_TYPE);
-
-export const MAGIC_ARRAY = {
-  BATCHED_MODEL: [98, 51, 100, 109],
-  INSTANCED_MODEL: [105, 51, 100, 109],
-  POINT_CLOUD: [112, 110, 116, 115],
-  COMPOSITE: [99, 109, 112, 116]
-};
-
-// TILE CONSTANTS
-
-// TODO - do we need this?
+// Copied from '@loaders.gl/3d-tiles'
 export const TILE3D_CONTENT_STATE = {
   UNLOADED: 0, // Has never been requested
   LOADING: 1, // Is waiting on a pending request
@@ -30,20 +8,7 @@ export const TILE3D_CONTENT_STATE = {
   FAILED: 5 // Request failed.
 };
 
-export const TILE3D_OPTIMIZATION_HINT = {
-  NOT_COMPUTED: -1,
-  USE_OPTIMIZATION: 1,
-  SKIP_OPTIMIZATION: 0
-};
-
-export const TILE3D_COLOR_BLEND_MODE = {
-  HIGHLIGHT: 0, // Multiplies the source color by the feature color.
-  REPLACE: 1, // Replaces the source color with the feature color.
-  MIX: 2 // Blends the source color and feature color together
-};
-
 export const TILE3D_REFINEMENT = {
   ADD: 0, // Render tile and, if screen space error exceeded, also refine to its children.
   REPLACE: 1 // Render tile or, if screen space error exceeded, refine to its descendants instead.
 };
-// SUBSET OF GL CONSTANTS - CAN BE USED DIRECTLY WITH WEBGL
